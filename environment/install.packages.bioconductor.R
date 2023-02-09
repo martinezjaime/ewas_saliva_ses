4aaa2a61fd1a5b6c1554b2796bc0d226db3bd6fb
martinezjaime <87441293+martinezjaime@users.noreply.github.com>
2023-02-09 13:29:24 -0500

    Add files via upload

---
+++ environment/install.packages.bioconductor.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+## install required R bioconductor packages, for ewas_saliva
+# install requiered bioconductor packages
+if (!requireNamespace("BiocManager", quietly = TRUE))
+	install.packages("BiocManager")
+library(BiocManager)
+BiocManager::install(c(
+			"minfi",
+			"IlluminaHumanMethylation450kmanifest",
+			"IlluminaHumanMethylationEPICmanifest",
+			"wateRmelon",
+			"readxl",
+			"RPMM",
+			"FlowSorted.Blood.450k",
+			"FlowSorted.Blood.EPIC",
+			"EpiDISH",
+			"bacon",
+			"sva",
+			"AnnotationDb",
+			"org.Hs.eg.db",
+			"ENmix"),
+			update = TRUE, ask = FALSE)
