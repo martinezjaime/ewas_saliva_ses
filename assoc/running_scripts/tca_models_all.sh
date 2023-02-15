164308ceac692fd2c140a8ad5e5f8bee439200a9
martinezjaime <87441293+martinezjaime@users.noreply.github.com>
2023-02-15 18:30:42 -0500

    Create tca_models_all.sh

---
+++ assoc/running_scripts/tca_models_all.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=running_tca_models_ewas_saliva_ses
+#SBATCH --out="slurm-%j.out"
+#SBATCH --time=04:00:00
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1
+#SBATCH --mem-per-cpu=32G
+#SBATCH --mail-type=ALL
+#SBATCH --partition=day
+####################################################################################
+# script for running the TCA models for saliva SES in children
+# day: 15 february 2023
+# analyzer: Jose Jaime Martinez-Magana
+# cluster: Grace - HPC Yale
+####################################################################################
+# This script uses the assoc_tca_450K.Rscript for linear models that could be found in the 
+# following github: https://github.com/martinezjaime/ewas_saliva_ses/tree/main/assoc/cell_specific
+####################################################################################
+# load miniconda
+module load miniconda
+# activate environment
+conda activate ewas_saliva
+# path to Rscript
+# this path should be change based on datapaths
+Rscript="/vast/palmer/scratch/montalvo-ortiz/jjm262/epigenomics/ewas_saliva_ses/script/assoc/cell_specific/assoc_tca_450K.Rscript"
+
+# running script for unrelated individuals
+Rscript $Rscript --file=/vast/palmer/scratch/montalvo-ortiz/jjm262/epigenomics/ewas_saliva_ses/databases/qced/qced_data_v02062023.rds \
+--out=/vast/palmer/scratch/montalvo-ortiz/jjm262/epigenomics/ewas_saliva_ses/results/assoc/cell_specific/ \
+--outname=tca_unrelated_v15022023.rds \
+--samplelist=/vast/palmer/scratch/montalvo-ortiz/jjm262/epigenomics/ewas_saliva_ses/samplelist/sample_subset_unrelated_v02142023.csv \
+--pcafile=/vast/palmer/scratch/montalvo-ortiz/jjm262/epigenomics/ewas_saliva_ses/pcs/glint_epi_children_v08February2023.epistructure.pcs.txt \
+--pheno=SES \
+--covar=age,gender \
+--sva=TRUE \
+--mprop=FALSE \
+--nsva=2 \
+--npcs=2
